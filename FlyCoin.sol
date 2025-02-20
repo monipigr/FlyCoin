969e6ce6072e15c42c9a6651dc2a078c8aabdcfb
monica.pigrau <monibixv2@gmail.com>
2025-02-20 23:09:16 +0100

    FlyCoin

--- FlyCoin.sol
+++ FlyCoin.sol
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: LGPL-3.0-only
-
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -7,11 +5,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FlyCoin is ERC20, Ownable {
 
-    uint public constant INITIAL_TOKENS = 15 * 1e18;
+    uint public constant INITIAL_TOKENS = 15 * 1e18; // Repartimos 15 tokens en el airdrop
     uint public constant VOTE_COST = 5 * 1e18; // Cada voto vale 5 tokens
 
     address[5] private voters;
     mapping(address => bool) private hasClaimed;
+    mapping(address => bool) private hasVoted;
+    mapping(address => bool) private hasDelegatedVote;
+    mapping(address => uint256) private voterAmount;
 
     constructor() ERC20('FlyCoin', 'FLY') Ownable(msg.sender) {
         _mint(msg.sender, ((5*15) * 1e18)); //0x5B38Da6a701c568545dCfcB03FcB875f56beddC4 owner
@@ -37,12 +38,19 @@ contract FlyCoin is ERC20, Ownable {
         _;
     }
 
+    modifier checkTokensAmount() {
+        require(balanceOf(msg.sender) >= VOTE_COST, "Not enough tokens to vote");
+        _;
+    }
+
+    event Voted(address indexed voter);
+
     // Los usuarios pueden reclamar su airdrop si están en la lista inicial y no lo han reclamado aun
     function claim() checkVoter(msg.sender) public {
         require(!hasClaimed[msg.sender], "You have alredy claimed your initial tokens");
 
         _transfer(owner(), msg.sender, INITIAL_TOKENS); //Transferimos del owner que tiene todos los tokens al sender que está reclamando su airdrop
-        hasClaimed[msg.sender] = true; //Seteamos el mappping hasClaimed
+        hasClaimed[msg.sender] = true;
     }
 
     // Bloqueamos transferencias entre usuarios
@@ -55,6 +63,36 @@ contract FlyCoin is ERC20, Ownable {
     // Bloqueamos transferencias entre usuarios
     function transferFrom(address /*from*/, address /*to*/, uint256 /*amount*/) public pure override returns (bool) {
         revert("Transfers between users are not allowed");
+    }
+
+    // Función para votar si no lo ha hecho ya con quema de tokens y reducción de tokens del votante. 
+    function vote() public checkTokensAmount() {
+        require(!hasVoted[msg.sender], "You have already voted");
+
+        hasVoted[msg.sender] = true; // Controlamos quién ha votado
+        _burn(msg.sender, VOTE_COST); // Quemamos los tokens
+        emit Voted(msg.sender);
+    }
+
+    // Hacemos que los votos sean delegables y qué este puede votar 2 veces (su voto y el que le ha delegado el voto)
+    function delegateVote(address to_) public checkVoter(to_) checkTokensAmount() {
+        require(!hasVoted[msg.sender], "You have already voted");
+        require(!hasDelegatedVote[msg.sender], "You have already delegated your vote");
+        require(to_ != msg.sender, "You cannot delegate to yourself");
+        
+        hasDelegatedVote[to_] = true;
+        hasVoted[msg.sender] = true; //Quitamos el derecho a voto del sender
+        _transfer(msg.sender, to_, VOTE_COST); // Transferimos los tokens al receptor
+    }
+
+    // Función para votar cuando tienesun voto delegado
+    function voteDelegated() public {
+        require(hasDelegatedVote[msg.sender], "You have not received a delegated vote");
+
+        hasDelegatedVote[msg.sender] = false;
+        hasVoted[msg.sender] = true;
+        _burn(msg.sender, VOTE_COST); //Quemamos los tokens
+        emit Voted(msg.sender);
     }
 
 }
